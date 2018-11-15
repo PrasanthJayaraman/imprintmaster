53e5ac881ee137ce944f655b6bc3f224687e4988
PrasanthJayaraman <prasanthj@outlook.com>
2018-11-15 23:15:56 +0530

    Employer creation & login is done

---
+++ controllers/user.js
@@ -0,0 +1,127 @@
+const common = require('../helpers/common');
+const User = require('../models/users');
+
+exports.create = function (req, res, next) {
+    var user = req.body;
+
+    if (!user || Object.keys(user).length === 0) {
+        return res.status(400).send({
+            message: "User data is required"
+        });
+    }
+
+    if (!user.phone || !user.email || !user.password || !user.name || !user.username) {
+        return res.status(400).send({
+            message: "Some Mandatory Field is Missing"
+        });
+    }    
+
+    // if(!common.validatePhone(user.phone)) {
+    //     return res.status(400).send({
+    //         message: "Invalid Phone Number"
+    //     });
+    // }
+
+    if (!common.validateEmail(user.email)) {
+        return res.status(400).send({
+            message: "Invalid Email address"
+        });
+    }
+
+    if (!common.validateUsername(user.username)) {
+        return res.status(400).send({
+            message: "User Name should be between 3 and 64 characters long"
+        });
+    }
+
+    User.lookUp(user, function (err, existingUser) {
+        if (err) {
+            return res.status(500).send({
+                message: "Error looking up User"
+            });
+        } else if (existingUser) {
+            if (existingUser.phone == user.phone) {
+                return res.status(409).send({
+                    message: "A User already availble with this phone number"
+                });
+            } else if (existingUser.email == user.email) {
+                return res.status(409).send({
+                    message: "A User already availble with this email address"
+                });
+            } else {
+                return res.status(409).send({
+                    message: "A User already availble with this username"
+                });
+            }
+        } else {
+            // Set false by default
+            user.verify = {}; 
+            user.verify.phone = false;
+            user.verify.email = false;
+
+            user.phoneOTP = common.generateOTPKey();
+            user.emailOTP = common.generateOTPKey();
+
+            user.active = false;
+            user.created = new Date();
+            user.modified = new Date();
+            user.registrationId = common.registration();
+            user.accessToken = common.rand();
+
+            User.create(user, function (err, userData) {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).send({
+                        message: "Error Creating User Profile"
+                    });
+                } else {
+                    return res.status(201).send({
+                        status: true,
+                        accessToken: userData.accessToken
+                    });
+                }
+            })
+        }
+    });    
+}
+
+exports.login = function(req, res, next){
+    var user = req.body;
+
+    if (!user || Object.keys(user).length === 0) {
+        return res.status(400).send({
+            message: "User data is required"
+        });
+    }
+
+    User.lookUp(user, function (err, userData) {
+        if (err) {
+            return res.status(500).send({
+                message: "Error looking up for User"
+            });
+        } else if (userData) {
+            if (!userData.password(user.password)) {
+                // Wrong passwowrd
+                return res.status(200).send({
+                    status: false                    
+                });
+            }
+            userData.createSession(function (err, loggedInUser) {
+                if (err) {
+                    return res.status(500).send({
+                        message: "Error in creating session"
+                    });
+                } else {
+                    return res.status(200).send({
+                        status: true,
+                        accessToken: userData.accessToken
+                    });
+                }
+            })
+        } else {
+            return res.status(200).send({
+                status: false
+            });
+        }
+    });
+}
---
+++ models/users.js
@@ -0,0 +1,231 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var common = require("../helpers/common");
+
+var verifySchema = new Schema({
+    phone: {
+        type: Boolean,
+        default: false
+    },
+    email: {
+        type: Boolean,
+        default: false
+    }
+});
+
+var addressSchema = new Schema({
+    street: {
+        type: String,
+        trim: true
+    },
+    city: {
+        type: String,
+        trim: true
+    },
+    state: {
+        type: String,
+        trim: true
+    },
+    pincode: {
+        type: Number,
+    },
+    country: {
+        type: String,
+        trim: true
+    }
+});
+
+var employeeSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    employeeId: {
+        type: String
+    },
+    photo: {
+        type: String,
+        trim: true
+    },
+    type: {
+        type: String,
+        required: true,
+        enum: ["manager", "leader", "employee"]
+    },
+    designation: {
+        type: String,
+        required: true
+    },
+    active: { // make inactiv if some companies cancels our services, incase if they come back we could have their data back.
+        type: Boolean,
+        default: true,
+        index: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        index: true
+    },
+    address: addressSchema,
+    _password: {
+        type: String,
+        required: true
+    },
+    _salt: { // encrypt & decrypt using salt
+        type: String
+    },
+    creadted: {
+        type: Date,
+        default: Date.now
+    },
+    modified: {
+        type: Date
+    },
+    accessToken: {
+        type: String,
+        index: true,
+        unique: true,
+        sparse: true
+    },
+    phoneVerificationKey: {
+        type: String
+    },
+    reportingTo: {
+        type: Schema.Types.ObjectId
+    },
+    verify: verifySchema
+});
+
+var userSchema = new Schema({
+    username: { // login user name for company head
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    registrationId: {
+        type: String,
+        required: true,
+        index: true
+    },
+    name: { // Main business name
+        type: String,
+        required: true,
+        trim: true
+    },
+    photo: {
+        type: String,
+        trim: true
+    },
+    active: { // make inactiv if some companies cancels our services, incase if they come back we could have their data back.
+        type: Boolean,
+        default: false,
+        index: true
+    },
+    landline: {
+        type: String,
+        trim: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        index: true
+    },
+    address: addressSchema,
+    _password: {
+        type: String,
+        required: true
+    },
+    _salt: { // encrypt & decrypt using salt
+        type: String
+    },
+    creadted: {
+        type: Date,
+        default: Date.now
+    },
+    modified: {
+        type: Date
+    },
+    accessToken: {
+        type: String,
+        index: true,
+        unique: true,
+        sparse: true
+    },
+    passwordResetKey: {
+        type: String,
+        index: true,
+        unique: true,
+        sparse: true //http://stackoverflow.com/a/21211640/1885921
+    },
+    passwordKeyValidTill: {
+        type: Date
+    },
+    phoneOTP: {
+        type: String
+    },
+    emailOTP: {
+        type: String
+    },
+    verify: verifySchema,
+    employees: [employeeSchema]
+}, {
+    collection: "user"
+});
+
+// Virtual Properties
+userSchema.virtual('password')
+    .get(function () {
+        return function (password) {
+            return (common.sha512(password + this._salt) === this._password);
+        }
+    })
+    .set(function (value) {
+        var salt = common.rand(512);
+        this._salt = salt;
+        this._password = common.sha512(value + salt);
+    });
+
+userSchema.statics.create = function (obj, callback) {
+    new this(obj).save(callback);
+};
+
+userSchema.statics.lookUp = function (obj, callback) {
+    this.findOne({
+        $or: [{
+            phone: obj.phone
+        }, {
+            email: obj.email
+        }, {
+            username: obj.username
+        }]
+    }, callback)
+}
+
+userSchema.methods.createSession = function (cb) {
+    this.modified = new Date();
+    this.accessToken = common.rand();
+    this.save(cb);
+};
+
+module.exports = User = mongoose.model("User", userSchema);
